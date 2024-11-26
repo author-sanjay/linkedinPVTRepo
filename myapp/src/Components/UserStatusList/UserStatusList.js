d10d59107036def17c71b0880883c9a11d14f505
sanjay <sanjaykumar73189@gmail.com>
2024-11-26 20:49:22 +0530

    UI state management

--- myapp/src/Components/UserStatusList/UserStatusList.js
+++ myapp/src/Components/UserStatusList/UserStatusList.js
@@ -1,31 +1,80 @@
-import React from 'react'
-import "./UserStatusList.css"
-import { CircleFadingPlus, MessageSquareText, Settings, Users } from "lucide-react"; 
-import UserAvatar from '../UserAvatar/UserAvatar';
-const UserStatusList = () => {
+import React from "react";
+import "./UserStatusList.css";
+import {
+  CircleFadingPlus,
+  CirclePlus,
+  MessageSquareText,
+  Settings,
+  Users,
+} from "lucide-react";
+import UserAvatar from "../UserAvatar/UserAvatar";
+const UserStatusList = ({ selectedOption, setSelectedOption }) => {
   return (
     <div className="userStatusListContainer">
       <div className="statusTopList">
-        <div className="singleIconContainer">
+        <div
+          className={`singleIconContainer ${
+            selectedOption === "Chat" ? "selected" : ""
+          }`}
+          onClick={() => {
+            setSelectedOption("Chat");
+          }}
+        >
           <MessageSquareText />
         </div>
-        <div className="singleIconContainer">
+        <div
+          className={`singleIconContainer ${
+            selectedOption === "Status" ? "selected" : ""
+          }`}
+          onClick={() => {
+            setSelectedOption("Status");
+          }}
+        >
           <CircleFadingPlus />
         </div>
-        <div className="singleIconContainer">
+        <div
+          className={`singleIconContainer ${
+            selectedOption === "Groups" ? "selected" : ""
+          }`}
+          onClick={() => {
+            setSelectedOption("Groups");
+          }}
+        >
           <Users />
+        </div>
+        <div
+          className={`singleIconContainer ${
+            selectedOption === "Add Chat" ? "selected" : ""
+          }`}
+          onClick={() => {
+            setSelectedOption("Add Chat");
+          }}
+        >
+          <CirclePlus />
         </div>
       </div>
       <div className="statusBottomList">
-        <div className="singleIconContainer">
+        <div
+          className={`singleIconContainer ${
+            selectedOption === "Settings" ? "selected" : ""
+          }`}
+          onClick={() => {
+            setSelectedOption("Settings");
+          }}
+        >
           <Settings />
         </div>
-        <div className="singleIconContainer">
+        <div
+          className="singleIconContainer"
+          onClick={() => {
+            setSelectedOption("Profile");
+          }}
+        >
           <UserAvatar />
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default UserStatusList
+export default UserStatusList;
--- myapp/src/Screens/ChatDashboard.js
+++ myapp/src/Screens/ChatDashboard.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import "./ChatDashboard.css"
-import UserStatusList from '../Components/UserStatusList/UserStatusList'
-import UserChatList from '../Components/UserChatList/UserChatList'
-import UserChatBox from '../Components/UserChatBox/UserChatBox'
+import React, { useState } from "react";
+import "./ChatDashboard.css";
+import UserStatusList from "../Components/UserStatusList/UserStatusList";
+import UserChatList from "../Components/UserChatList/UserChatList";
+import UserChatBox from "../Components/UserChatBox/UserChatBox";
 const ChatDashboard = () => {
+  const [selectedOption, setSelectedOption] = useState("Chat");
+  
   return (
-    <div className='homeContainer'>
-        <div className='chatBoxContainer'>
-            <UserStatusList/>
-            <UserChatList/>
-            <UserChatBox/>
-        </div>
+    <div className="homeContainer">
+      <div className="chatBoxContainer">
+        <UserStatusList
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+        />
+        <UserChatList />
+        <UserChatBox />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatDashboard
+export default ChatDashboard;
